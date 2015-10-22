cc0478cb40281bf6e92610ebad8ce5d3f14d8705
Erlend Aune <erlend.aune@amedia.no>
2015-10-22 12:30:36 +0200

    .

--- src/process-json.py
+++ src/process-json.py
@@ -7,8 +7,42 @@ import re
 import matplotlib.pyplot as plt
 import numpy as np
 
-PAR_RE = re.compile(r'<.?p>')
+PAR_RE = re.compile(r'<p>')
 TAG_RE = re.compile(r'<[^>]+>')
+
+GET_ACPID=re.compile(r'/([0-9]+\-[0-9]+\-[0-9]+)\.')
+
+def process_body(txt):
+    re_txt=PAR_RE.sub('\n',txt)
+    re_txt=TAG_RE.sub('',re_txt)
+    return re_txt
+
+def get_acpids(src):
+    acpids=[]
+    if '_alternates' in src:
+        for link in src['_alternates']:
+            acpid=GET_ACPID.findall(link)[0]
+            acpids.append(acpid)
+    if len(acpids)>0:
+        return acpids
+    else:
+        return False
+
+def get_analysis_data(dd):
+    indexname=dd['_index']
+    idcontent=dd['_id']
+    if '_source' not in dd:
+        return False
+    src=dd['_source']
+    acpids=get_acpids(src)
+    
+    if acpids:
+        fns=acpids
+    else:
+        fns=[indexname+'-'+idcontent]
+
+
+
 
 
 
@@ -16,18 +50,26 @@ files = glob.glob('../data/*.json')
 caseLengths=[]
 for pp in files:
     with open(pp) as ff:
+
         jsonObj=json.load(ff)
         for jo in jsonObj:
-            jo=jo['_source']
-            if 'body' in jo['fields']:
-                caseLengths.append(len(jo['fields']['body']))
-                if len(jo['fields']['body'])>200:
-                    jo['fields']['body']=PAR_RE.sub('\n',jo['fields']['body'])
-                    jo['fields']['body']=TAG_RE.sub('',jo['fields']['body'])
-                    jo['fields']['body']=jo['fields']['body'].strip()
+            fns=[]
+            try:
+                acpids=[]
+                for link in jo['_source']['_alternates']:
+                    acpid=GET_ACPID.findall(link)[0]
+                    acpids.append(acpid)
+                    fns.append(acpid)
 
-                    # pprint(jo)
-                    # input()
+            except exception as E:
+                fns.extend(jo['_index']+'-'+jo['_id'])
+            try:
+                ret_obj={'body':process_body(jo['_source'][''])}
+            except:
+                continue
+            ret_obj['id']=jo['_id']
+            ret_obj['acp-id']=
+
 
 
 caseLengths=np.array(caseLengths)
